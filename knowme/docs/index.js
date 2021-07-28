a1b74338d66407cd92059f789c59caa5bd072ed0
Hulusi Bumin Yalçın <70579311+HBuminY@users.noreply.github.com>
2021-07-28 17:07:48 +0300

    Delete knowme/docs directory

--- knowme/docs/index.js
+++
@@ -1,9 +0,0 @@
-const ww = window.innerWidth; const wh = window.innerHeight;
-function setup() {
-    createCanvas(ww, wh);
-}
-
-function draw() {
-    background(220);
-    ellipse(50, 50, 80, 80);
-}
