aecf4990c8719b49b97c44691f39da666449bb12
HBuminY <70579311+HBuminY@users.noreply.github.com>
2021-07-28 22:47:57 +0300

    complated home page

--- docs/index.js
+++ docs/index.js
@@ -6,20 +6,23 @@ texts = [
 ];
 var txtIndex=0;
 var txt = texts[txtIndex];
-var speed = 500;
+var OriSpeed = 300;
+var speed = OriSpeed
 
 function twWrite(){
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
+    i++; if(speed>100){speed-=50};
     setTimeout(twWrite, speed);
-  }else if(i == txt.length){twRemove()};
+  }else if(i == txt.length){speed = OriSpeed; console.log("removing"); setTimeout(twRemove, 2000)};
 };
 
 function twRemove(){
     if (i > -1) {
       document.getElementById("demo").innerHTML = txt.slice(0, i);
-      i--;
+      i--; if(speed>100){speed-=50};
       setTimeout(twRemove, speed);
-    }else if(i == -1){txtIndex++; txt=texts[txtIndex%3]; twWrite();};
-  };
+    }else if(i == -1){speed = OriSpeed; txtIndex++; txt=texts[txtIndex%3]; twWrite();};
+};
+
+document.onload = twWrite();
