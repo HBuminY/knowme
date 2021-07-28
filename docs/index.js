88efbd01c68c6ac1a44c9c30e3d854cbce0cc25f
Hulusi Bumin Yalçın <70579311+HBuminY@users.noreply.github.com>
2021-07-28 17:07:22 +0300

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+const ww = window.innerWidth; const wh = window.innerHeight;
+function setup() {
+    createCanvas(ww, wh);
+}
+
+function draw() {
+    background(220);
+    ellipse(50, 50, 80, 80);
+}
